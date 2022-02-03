57d9d7108ad63c26b1de166972d618c172889113
chen <1834805770@qq.com>
2022-02-03 11:30:00 +0800

    测试类型声明是否生效

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -2,8 +2,12 @@
   <div>
     <h2>Hello World</h2>
     <p>{{ counts }}</p>
+    <p>{{ message }}</p>
     <button @click="counts += 1">
       点击+1
+    </button>
+    <button @click="message = message === 'hello' ? 'hi' : 'hello'">
+      点击打招呼
     </button>
   </div>
 </template>
@@ -12,6 +16,7 @@
 import { ref } from '@nuxtjs/composition-api'
 // your script setup
 const counts = ref(0)
+const message = ref<'' | 'hi' | 'hello'>('')
 </script>
 
 <script lang="ts">
