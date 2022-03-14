3f26ed1ba77e789b010cd34d0d1bd0ac7a3b48a8
uphg <2633565885@qq.com>
2022-03-14 11:12:50 +0800

    添加 sass 预处理器

--- components/CountNumber.vue
+++ components/CountNumber.vue
@@ -2,6 +2,7 @@
   <div>
     <p>{{ number }}</p>
     <p>{{ multiple }}</p>
+    <button class="btn">普通按钮</button>
   </div>
 </template>
 
@@ -21,3 +22,19 @@ defineProps({
 
 defineEmits(['2', '3'])
 </script>
+
+<style lang="scss">
+.btn {
+  cursor: pointer;
+  border: 0;
+  color: #fff;
+  padding: 8px 16px;
+  background-color: #485fc7;
+  &:hover, &:focus {
+    background-color: #3e56c4;
+  }
+  &:active {
+    background-color: #3a51bb;
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -37,8 +37,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     ['@nuxt/typescript-build', { typeCheck: false }],
-    '@nuxtjs/composition-api/module',
-    'unplugin-vue2-script-setup/nuxt'
+    '@nuxtjs/composition-api/module'
   ],
 
   scriptSetup: { /* options */ },
